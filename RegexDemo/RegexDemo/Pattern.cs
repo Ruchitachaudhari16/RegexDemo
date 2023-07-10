fc9199856976cf2c44a39ca69f7e47c47ce717ce
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-07-10 08:49:38 -0400

    [Ruchita]Add: String pattern

---
+++ RegexDemo/RegexDemo/Pattern.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexDemo
+{
+  public  class Pattern
+    {
+        static string pattern = "^cen$";
+        //^ it will indicate start of string.
+        //$ it will indicate the end of string.
+
+        public void MyRegex()
+        {
+            Regex regex=new Regex(pattern);
+            if (regex.IsMatch("kiran"))
+            {
+                Console.WriteLine("String is match the pattern ");
+            }
+            else 
+            {
+                Console.WriteLine("string does not match the pattern ");
+            }
+        }
+    }
+}
---
+++ RegexDemo/RegexDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexDemo
+{
+  public class Program
+    {
+        static void Main(string[] args)
+        {
+            Pattern pattern = new Pattern();
+            pattern.MyRegex();
+            Console.ReadLine();
+        }
+    }
+}
